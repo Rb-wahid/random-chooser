1d05985a98d41680dd44f15385bb64a77a0fe600
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2022-03-26 15:53:33 +0600

    responsive for mobile

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
+import Header from "./components/Header/Header";
 import UI from "./components/UI/UI";
 
 function App() {
   return (
-    <>
+    <div className="App">
+      <Header/>
       <UI />
-    </>
+    </div>
   );
 }
 
