0b3bad64aa976f32746adeb2e2d6803f714fe1ad
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2022-03-26 19:18:12 +0600

    answer update

--- src/components/QnA/QnA.js
+++ src/components/QnA/QnA.js
@@ -9,21 +9,21 @@ const QnA = () => {
         <div>
           <h2>How useState() work?</h2>
           <p>
-            we can use hooks only in functional component. Basically, useState
-            is a special function that return an array of two entries. one is
-            stored value, it is state and another one is a function. we can
-            update the state value using that function.useState take initial
-            state as an argument. we can only call useState at top level and can
-            not call it inside loops, conditions, or nested functions.
+            We can use hooks only in functional component. Basically, useState
+            is a special function that returns an array of two entries. One is
+            stored value, it is state and another one is a function. We can
+            update the state using that function. useState takes initial state
+            as an argument. We can only call useState at top level and can not
+            call it inside loops, conditions, or nested functions.
           </p>
         </div>
         <div>
           <h2>Props VS State</h2>
           <strong>Props : </strong>
           <p>
-            we can not change the props. props are read-only and
-            immutable. using props, we can pass data from one to another
-            components and It's make components reusable.
+            We cannot change the props. Props are read-only and immutable. Using
+            props, we can pass data from one to another components and It's made
+            components reusable.
           </p>
           <strong>State : </strong>
           <p>
